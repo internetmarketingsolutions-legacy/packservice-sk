2a9a3b152d66f79d8fc548ea198fe7ea6a840572
Dominik Zogg <dominik.zogg@gmail.com>
2016-03-22 22:18:37 +0100

    there is no need for a self calling closure

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,9 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-call_user_func(function() {
-    if ( ! is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
-        throw new \RuntimeException('Did not find vendor/autoload.php. Did you run "composer install --dev"?');
-    }
+if ( ! is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \RuntimeException('Did not find vendor/autoload.php. Did you run "composer install --dev"?');
+}
 
-    $loader = require $autoloadFile;
-    AnnotationRegistry::registerLoader('class_exists');
-});
+$loader = require $autoloadFile;
+AnnotationRegistry::registerLoader('class_exists');
